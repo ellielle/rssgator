f736ae4543c96d75170ae4e4c512664183d8d896
ellielle <40385743+ellielle@users.noreply.github.com>
2024-10-21 12:53:05 -0400

    add functions to handle reading and writing of config

---
+++ internal/config/config.go
@@ -0,0 +1,97 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+const configFile = ".gatorconfig.json"
+
+const DBURL = "postgres://example"
+
+// Read opens the config file and parses the content
+// into a Config struct that is returned
+func Read() (Config, error) {
+	filePath, err := getConfigPath()
+	if err != nil {
+		return Config{}, err
+	}
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		createConfigFile()
+		fmt.Println("creating configuration, re-run the program")
+		os.Exit(1)
+	}
+
+	var cfg Config
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return Config{}, errors.New("error unmarshaling config")
+	}
+
+	return cfg, nil
+}
+
+// createConfigFile creates a config file with the default information
+func createConfigFile() error {
+	config := Config{
+		DBURL: DBURL,
+	}
+	err := write(&config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// getConfigPath returns the user's rssgator config file path
+func getConfigPath() (string, error) {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.New("could not find home directory")
+	}
+	return dir + "/" + configFile, nil
+}
+
+// SetUser sets the current user of the database and saves it to the config
+func (cfg *Config) SetUser() error {
+	user, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+	}
+	cfg.CurrentUserName = user
+
+	err = write(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func write(cfg *Config) error {
+	filePath, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+
+	config, err := json.Marshal(cfg)
+	if err != nil {
+		return errors.New("could not marshal configuration")
+	}
+	// write the config to file
+	err = os.WriteFile(filePath, config, 0600)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return nil
+}
