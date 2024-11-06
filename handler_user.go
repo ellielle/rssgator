7ce1107fddfdf21355fa141ee0e32540d3331cab
ellielle <40385743+ellielle@users.noreply.github.com>
2024-11-06 12:59:39 -0500

    move run and register functions into main
    makes more sense

--- handler_user.go
+++ handler_user.go
@@ -86,17 +86,3 @@ func handlerGetUsers(st *state, _ command) error {
 	}
 	return nil
 }
-
-// register adds commands to the commands struct
-func (c *commands) register(name string, f func(*state, command) error) {
-	c.registeredCommands[name] = f
-}
-
-// run ensures a command has been registered, and then runs it
-func (c *commands) run(s *state, cmd command) error {
-	f, ok := c.registeredCommands[cmd.Name]
-	if !ok {
-		log.Fatalf("%s is not a command", cmd.Name)
-	}
-	return f(s, cmd)
-}
